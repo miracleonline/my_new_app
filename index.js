27b34a449c155bf38783f51b7e02f29c69fc2747
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-12-14 05:01:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import { AppRegistry } from 'react-native';
-import App from './App'; // Your main App component
+import App from './App';
 import { name as appName } from './app.json';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 
-// Get the root element for the web
-const rootTag = document.getElementById('root');
-
-// If running on the web, use React DOM
-if (rootTag) {
-  const root = createRoot(rootTag); // React 18+
-  root.render(<App />);
-} else {
-  // For native platforms, use AppRegistry
-  AppRegistry.registerComponent(appName, () => App);
-}
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
