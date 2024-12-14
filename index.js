c994737004fbf9f15802f0131b1ca30e3db32e11
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-12-14 06:17:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ import App from './App';
 import { name as appName } from './app.json';
 import { render } from 'react-dom';
 
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById('root'),
-});
+// For React Native Web
+import { Platform } from 'react-native';
+import { createRoot } from 'react-dom/client';
+
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root');
+  const root = createRoot(rootTag);
+  root.render(<App />);
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+  AppRegistry.runApplication(appName, {
+    initialProps: {},
+    rootTag: document.getElementById('root'),
+  });
+}
